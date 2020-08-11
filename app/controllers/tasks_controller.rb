0fe264587b05776895f4926259baa45295c71334
Taoreed Lawal lwlgbenga@hotmail.com <lwlgbenga@hotmail.com>
2020-08-12 00:19:50 +0100

    views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,25 @@
 class TasksController < ApplicationController
+
+  def index
+    Task.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
